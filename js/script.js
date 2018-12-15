0f6db42ad6f4afb86e5a41e2208cbdb4ab44aec8
flatdesign <35264427+flatdesign@users.noreply.github.com>
2018-12-15 20:06:29 +0300

    Слайдер

--- js/script.js
+++ js/script.js
@@ -1,4 +1,46 @@
-$(function(){
+$(function () {
 
+  var $items = $('.reviews__list .reviews__element');
+
+  $('.reviews__list').owlCarousel({
+      nav: true,
+      loop: true,
+      margin: 80,
+      responsiveClass: true,
+      autoWidth: true,
+      dots: false,
+      responsive: {
+          0: {
+              items: 1
+          },
+          1000: {
+              items: 2
+          }
+      },
+      onInitialized: function () {
+          equalsHeight($items);
+      },
+      onResized: function () {
+          equalsHeight($items);
+    },
+  });
+
+  function equalsHeight($items) {
+      var max = 0;
+
+      $items.each(function () {
+          var h = $(this).find('> div').outerHeight();
+
+          if (h > max) {
+              max = h;
+          }
+      });
+
+      $items.css('min-height', max + 'px');
+
+      setTimeout(function () {
+          $('.reviews').trigger('refresh.owl.carousel');
+      }, 1);
+  }
 
 });
